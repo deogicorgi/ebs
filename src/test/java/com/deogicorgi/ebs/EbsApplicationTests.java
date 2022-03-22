713750807ba4fc0d9d85ec9298be59e905114f1d
deogicorgi <deogicorgi@gmail.com>
2022-03-23 07:30:04 +0900

    Init project

---
+++ src/test/java/com/deogicorgi/ebs/EbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deogicorgi.ebs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
