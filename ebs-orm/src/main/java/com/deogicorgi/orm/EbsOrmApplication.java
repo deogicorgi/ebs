7c23a930940599953866eb4672d37d4673470bef
deogicorgi <deogicorgi@gmail.com>
2022-03-23 21:28:29 +0900

    ORM API 모듈 추가

---
+++ ebs-orm/src/main/java/com/deogicorgi/orm/EbsOrmApplication.java
@@ -0,0 +1,13 @@
+package com.deogicorgi.orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbsOrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbsOrmApplication.class, args);
+    }
+
+}
